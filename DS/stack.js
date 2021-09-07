23f1a7a53ecde476a9ea6296745ab2ae6d576023
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2021-09-07 23:43:43 +0530

    stack implementation non array

---
+++ DS/stack.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.size = 0;
+    this.first = null;
+    this.last = null;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+
+    this.size++;
+    return this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    const removedVal = this.first.val;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return removedVal;
+  }
+}
